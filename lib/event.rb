dca3202fc7580b5f9ad4e6e1eaf83c06000e6641
agnesarth <agnesarth@hotmail.fr>
2020-04-20 13:07:56 +0200

    Petits changements events

--- lib/event.rb
+++ lib/event.rb
@@ -4,9 +4,11 @@ require 'time'
 class Event
     attr_accessor :start_date #, :duration, :title, :attendees
 
-    def initialize (start_date_to_save)
+    def initialize (start_date_to_save)#, duration_to_save, title_to_save)
         @start_date = Time.parse(start_date_to_save)
         # .strftime("%H:%M:%S %d/%m/%Y") pour le passer en format européen
+        #@duration = duration_to_save.to_i
+        #@title = title_to_save
     end
 
     binding.pry
